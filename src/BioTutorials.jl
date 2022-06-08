147271ca4df8cf00d365271f296c10a2fdd0ce5c
Kevin Bonham <kevin@bonham.ch>
2022-06-08 12:47:12 -0400

    make build function

--- src/BioTutorials.jl
+++ src/BioTutorials.jl
@@ -2,10 +2,16 @@ module BioTutorials
 
 function build(dir)
     isdir(dir) || throw(ArgumentError("Provide path to directory with tutorial file"))
-    files = filter(f-> splitext(f)[2] == ".jl", readdir(dir))
-    file = length(files) == 1 ? first(file) : throw(ArgumentError("Tutorial directory must have exactly one .jl file"))
+    files = filter(f-> splitext(f)[2] == ".jl", readdir(dir; join=true))
+    file = length(files) == 1 ? first(files) : throw(ArgumentError("Tutorial directory must have exactly one .jl file"))
 
-    run(`julia --project=@. -e 'using Literate; using Pkg; Pkg.activate($dir); Pkg.instantiate(); Literate.notebook($file, $name)'`)
+    c = Cmd([
+        "julia", "--project=@.", "-e",
+        """
+        using Literate; using Pkg; Pkg.activate("$dir"); Pkg.instantiate(); Literate.notebook("$file", "$dir")
+        """
+    ])
+    run(c)
 end
 
 end
